a4964f681376862bb9af11944c2b2a51756cac00
Maria Dobrydnyk <mariadobrydnyk@MacBook-Air-Maria.local>
2026-02-26 14:30:19 +0200

    У файлі index.js реалізовано крім надсилання файлу index.ejs з HTML-кодом ще списку доданих завдань у відповідь при відвідуванні URL-адреси root(/)

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 let express = require('express');
+let bodyParser = require("body-parser");
 let app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-app.get('/', function (req, res) {
-    res.render("index");
+
+let task = ["Опанувати JavaScript", "Опанувати Node.js"];
+app.post("/addnewtask", function (req, res) {
+    let newTask = req.body.newtask;
+    task.push(newTask);
+    res.redirect("/");
 });
 
-app.post('/addnewtask', function (req, res) {
-    res.render('index')
+
+app.get("/", function (req, res) {
+    res.render("index", { task: task });
 });
-  
+
 app.listen(3000, function() {
     console.log("Сервер працює на порту 3000!");
 });
+
+
