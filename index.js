f7ac2b0cddf288f91369ad4d410ffef165c58c0b
Maria Dobrydnyk <mariadobrydnyk@MacBook-Air-Maria.local>
2026-02-26 14:35:09 +0200

    У файлі index.js реалізовано крім надсилання файлу index.ejs з HTML-кодом та списку доданих завдань ще списку виконаних завдань у відповідь при відвідуванні URL-адреси root(/)

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ app.set("view engine", "ejs");
 
 let task = ["Опанувати JavaScript", "Опанувати Node.js"];
 app.post("/addnewtask", function (req, res) {
-    let newTask = req.body.newtask;
-    task.push(newTask);
+    let completeTask = req.body.check;
+    if (typeof completeTask === "string") {
+        complete.push(completeTask);
+        task.splice(task.indexOf(completeTask), 1);
+    } else if (typeof completeTask === "object") {
+        for (let i = 0; i < completeTask.length; i++) {
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]), 1);
+        }
+    }
     res.redirect("/");
 });
 
-
 app.get("/", function (req, res) {
-    res.render("index", { task: task });
+    res.render("index", { task: task, complete: complete });
 });
 
 app.listen(3000, function() {
